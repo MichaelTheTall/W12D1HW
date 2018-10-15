c4b5bbeb3d1cf62e2d7737f27dfb37f70c6e862c
MichaelTheTall <michaeljhnewman@gmail.com>
2018-10-15 21:53:05 +0100

    manager test

---
+++ src/test/java/ManagerTest.java
@@ -0,0 +1,47 @@
+import management.Manager;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ManagerTest {
+
+    Manager manager;
+
+    @Before
+    public void before(){
+        manager = new Manager("Bob", "AB123456C", 20000.00, "HR");
+    }
+
+    @Test
+    public void getName(){
+        assertEquals("Bob", manager.getName());
+    }
+
+    @Test
+    public void getNI(){
+        assertEquals("AB123456C", manager.getNI());
+    }
+
+    @Test
+    public void getSalary(){
+        assertEquals(20000.00, manager.getSalary(), 0.01);
+    }
+
+    @Test
+    public void getDeptName(){
+        assertEquals("HR", manager.getDeptName());
+    }
+
+    @Test
+    public void raiseSalary(){
+        manager.raiseSalary(2000.00);
+        assertEquals(22000.00, manager.getSalary(), 0.01);
+    }
+
+    @Test
+    public void payBonus(){
+        assertEquals(200.00, manager.payBonus(), 0.01);
+    }
+
+}
