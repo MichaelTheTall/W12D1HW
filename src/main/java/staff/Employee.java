edf11bc8cfebd25ffb6053779f2089f5ead77bad
MichaelTheTall <michaeljhnewman@gmail.com>
2018-10-15 21:33:32 +0100

    initticommitti

---
+++ src/main/java/staff/Employee.java
@@ -0,0 +1,16 @@
+package staff;
+
+public abstract class Employee {
+    private String name;
+    private String NI;
+    private int salary;
+
+    public Employee(String name, String NI, int salary) {
+        this.name = name;
+        this.NI = NI;
+        this.salary = salary;
+    }
+
+
+
+}
